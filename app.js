d93a801ed6aecaa20a143169cb189a85f79e4aa7
CN24 <cn24@Dans-MacBook-Pro-2.local>
2019-08-29 15:19:42 +0100

    added button click

--- app.js
+++ app.js
@@ -67,3 +67,13 @@ document.addEventListener("keydown", event => {
 
 
 // Play music by mouse click
+
+const aKeyPress = () => {sound.src = './sounds/clap.wav'; sound.play()}
+const sKeyPress = () => {sound.src = './sounds/hihat.wav'; sound.play()}
+const dKeyPress = () => {sound.src = './sounds/kick.wav'; sound.play()}
+const fKeyPress = () => {sound.src = './sounds/openhat.wav'; sound.play()}
+const gKeyPress = () => {sound.src = './sounds/boom.wav'; sound.play()}
+const hKeyPress = () => {sound.src = './sounds/ride.wav'; sound.play()}
+const jKeyPress = () => {sound.src = './sounds/snare.wav'; sound.play()}
+const kKeyPress = () => {sound.src = './sounds/tom.wav'; sound.play()}
+const lKeyPress = () => {sound.src = './sounds/tink.wav'; sound.play()}
